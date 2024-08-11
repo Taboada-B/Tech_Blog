ab77237ee06796944193b684453f557805a93eaf
Bryan Taboada <b.taboada123@gmail.com>
2024-08-11 11:56:57 -0600

    adding more routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,13 @@
 const router = require('express').Router();
 const userRoutes = require('./api/userRoutes');
 const postRoutes = require('./api/postRoutes'); 
-const homeRoutes = require('./homeRoutes')
+const homeRoutes = require('./homeRoutes');
+const authRoutes = require('./authRoutes');
 
-router.use('/',homeRoutes);
+router.use('/', homeRoutes);
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes); 
+router.use('/login', authRoutes); 
+router.use('/signup', authRoutes); 
 
 module.exports = router;

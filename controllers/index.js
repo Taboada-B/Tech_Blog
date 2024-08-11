8058737b334daee75c2df77a5df398f2a9579be9
Bryan Taboada <b.taboada123@gmail.com>
2024-08-11 09:57:08 -0600

    server.js changed console.log, added homeRoutes & changed index.js, basic login/main handlebars

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const userRoutes = require('./api/userRoutes');
 const postRoutes = require('./api/postRoutes'); 
+const homeRoutes = require('./homeRoutes')
 
+router.use('/',homeRoutes);
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes); 
 

bbb57d9ed3408912b512d23d2dc68da89f7aed1c
Bryan Taboada <b.taboada123@gmail.com>
2024-08-24 09:26:33 -0600

    correcting index routers

--- controllers/index.js
+++ controllers/index.js
@@ -1,15 +1,9 @@
 const router = require('express').Router();
 
-
-const userRoutes = require('./api/userRoutes');
-const postRoutes = require('./api/postRoutes'); 
+const apiRoutes = require('./api/index');
 const homeRoutes = require('./homeRoutes');
-const authRoutes = require('../utils/auth');
 
 router.use('/', homeRoutes);
-router.use('/users', userRoutes);
-router.use('/posts', postRoutes); 
-router.use('/login', authRoutes); 
-router.use('/signup', authRoutes); 
+router.use('/api', apiRoutes);
 
 module.exports = router;
